3e6b97917a32df9b0a977ace78dd7a618c80866c
Aman4563 <141989801+Aman4563@users.noreply.github.com>
2024-03-31 12:35:09 +0530

    Update private.client.js

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import queryString from "query-string";
 
-// const baseURL = "https://moonflix-api.vercel.app/api/v1/";
-const baseURL = "https://plhub-fix-api.vercel.app/api/v1/";
+const baseURL = "https://moonflix-api.vercel.app/api/v1/";
+// const baseURL = "https://plhub-fix-api.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
