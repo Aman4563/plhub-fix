390ef657ec86bae87c9b10687f17fbf1e93ce8f4
GRTWebsiteDevelopment <aman.yadav@greenridertech.com>
2024-12-01 20:22:12 +0530

    changed the backend url to the production one from the local.

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -5,8 +5,8 @@ import queryString from "query-string";
  * Base URL for the API requests
  * Update this value depending on the environment (e.g., production or development).
  */
-// const baseURL = "https://plhub-fix-api.vercel.app/api/v1/";
-const baseURL = "http://localhost:8000/api/v1/";
+const baseURL = "https://plhub-fix-api-git-advancefeature-9703ab-amans-projects-62ecaac6.vercel.app/api/v1/";
+// const baseURL = "http://localhost:8000/api/v1/";
 
 /**
  * Create an Axios instance with default configurations.
